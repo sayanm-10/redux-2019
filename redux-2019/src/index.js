9bdb8e5316efa504c7a99b22098f43537968157d
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-12-18 12:14:07 -0500

    add create-react-app

---
+++ redux-2019/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>Hola!</div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
