ab60bc6fa7366d79c12032347ff175384dfb3d37
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-12-19 12:50:41 -0500

    generate comments using semantic-ui and faker.js

--- redux-2019/src/index.js
+++ redux-2019/src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
-    return <div>Hola!</div>;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        John Doe
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00 PM</span>
+                    </div>
+                    <div className="text">Great post!</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
