79b21c008eeba915f801468ebe63a29ef81677e8
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-12-21 12:54:53 -0500

    add comment detail as child component to approval card

--- redux-2019/src/index.js
+++ redux-2019/src/index.js
@@ -8,31 +8,38 @@ import CommentDetail from "./components/CommentDetail";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="John Doe"
-                avatar={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                postTime="5.00 PM"
-            />
-            <CommentDetail
-                author="Jane Doe"
-                avatar={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                postTime="5.05 PM"
-            />
-            <CommentDetail
-                author="Jill Doe"
-                avatar={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                postTime="6.10 PM"
-            />
-            <CommentDetail
-                author="Kevin Doe"
-                avatar={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                postTime="6.20 PM"
-            />
-            <ApprovalCard />
+            <ApprovalCard>
+                <CommentDetail
+                    author="John Doe"
+                    avatar={faker.image.avatar()}
+                    comment={faker.lorem.sentence()}
+                    postTime="5.00 PM"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane Doe"
+                    avatar={faker.image.avatar()}
+                    comment={faker.lorem.sentence()}
+                    postTime="5.05 PM"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jill Doe"
+                    avatar={faker.image.avatar()}
+                    comment={faker.lorem.sentence()}
+                    postTime="6.10 PM"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Kevin Doe"
+                    avatar={faker.image.avatar()}
+                    comment={faker.lorem.sentence()}
+                    postTime="6.20 PM"
+                />
+            </ApprovalCard>
         </div>
     );
 };
