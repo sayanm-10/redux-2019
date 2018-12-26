80ddde026398ebde9442021911d94943ea20e1fa
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-12-26 11:48:08 -0500

    add lat to state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null
+        };
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                console.log(position);
+                this.setState({ lat: position.coords.latitude });
+            },
             err => console.log(err)
         );
+    }
 
-        return <div>HOLA!</div>;
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
