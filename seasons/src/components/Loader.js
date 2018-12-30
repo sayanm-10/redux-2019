3b5db1280bd26b3f2e296346ac8b416886846006
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-12-30 17:05:37 -0500

    use default props

--- seasons/src/components/Loader.js
+++ seasons/src/components/Loader.js
@@ -3,11 +3,13 @@ import React from "react";
 const Loader = props => {
     return (
         <div className="ui active dimmer">
-            <div className="ui big text loader">
-                {props.loadingText || "Loading..."}
-            </div>
+            <div className="ui big text loader">{props.loadingText}</div>
         </div>
     );
+};
+
+Loader.defaultProps = {
+    loadingText: "Loading..."
 };
 
 export default Loader;
