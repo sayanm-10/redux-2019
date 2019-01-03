381b68f591cb46eba9db3cd6655e321fa775c2e6
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-03 14:51:36 -0500

    show images as tiles using Refs and css spans

--- album/src/components/ImageCard.js
+++ album/src/components/ImageCard.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 
 export class ImageCard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            spans: 0
+        };
+
+        this.imageRef = React.createRef();
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans });
+    };
+
+    componentDidMount() {
+        // setTimeout(() => {
+        //     console.log(this.imageRef.current.clientHeight);
+        // }, 0);
+
+        // as images are being fetched, the current obj may not be available immediately
+        this.imageRef.current.addEventListener("load", this.setSpans);
+    }
+
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div>
-                <img src={urls.regular} alt={description} />
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img ref={this.imageRef} src={urls.regular} alt={description} />
             </div>
         );
     }
