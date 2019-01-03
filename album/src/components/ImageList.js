5a192992d589e0b2bee0304ca9207ca12a101248
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-03 13:28:04 -0500

    show images v2

--- album/src/components/ImageList.js
+++ album/src/components/ImageList.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 const ImageList = props => {
-    const images = props.images.map(image => {
-        return <img key={image.id} src={image.urls.regular} />;
+    const images = props.images.map(({ id, urls, description }) => {
+        return (
+            // <img
+            //     key={image.id}
+            //     src={image.urls.regular}
+            //     alt={image.description}
+            // />
+
+            // Alternate by using destructured args
+            <img key={id} src={urls.regular} alt={description} />
+        );
     });
 
     return <div>{images}</div>;
