b1970642fbbff04f1066aec1da4229e9dae4ecf6
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-09 16:17:57 -0500

    implement redux-thunk middleware

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,22 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-    const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => {
+    return async dispatch => {
+        const response = await jsonPlaceholder.get("/posts");
 
-    return {
-        type: "FETCH_POSTS",
-        payload: response
+        dispatch({
+            type: "FETCH_POSTS",
+            payload: response
+        });
     };
 };
+
+// * Equivalent to
+// export const fetchPosts = () =>  async dispatch => {
+//     const response = await jsonPlaceholder.get("/posts");
+
+//     dispatch({
+//         type: "FETCH_POSTS",
+//         payload: response
+//     });
+// };
