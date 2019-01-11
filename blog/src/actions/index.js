e33adad5f6266b0cb3a9a5465004d3b2e8a4fe2e
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-11 12:26:16 -0500

    refactor action creators

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, "userId"));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => {
     return async dispatch => {
         const response = await jsonPlaceholder.get("/posts");
@@ -22,18 +29,29 @@ export const fetchPosts = () => {
 //     });
 // };
 
-// ! Memoized Approach
 export const fetchUser = id => {
-    return dispatch => {
-        _fetchUser(id, dispatch);
+    return async dispatch => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({
+            type: "FETCH_USER",
+            payload: response.data
+        });
     };
 };
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// ! Memoized Approach
+// export const fetchUser = id => {
+//     return dispatch => {
+//         _fetchUser(id, dispatch);
+//     };
+// };
 
-    dispatch({
-        type: "FETCH_USER",
-        payload: response.data
-    });
-});
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({
+//         type: "FETCH_USER",
+//         payload: response.data
+//     });
+// });
