eb154ce6d78557f5e27e79513f4dd833b142deef
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-23 15:47:14 -0500

    refactor the context to work as a store

--- context/src/components/App.js
+++ context/src/components/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from "react";
-import LanguageContext from "../context/LanguageContext";
+import { LanguageStore } from "../context/LanguageContext";
 import ColorContext from "../context/ColorContext";
 
 import UserCreate from "./UserCreate";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends Component {
-    state = {
-        language: "en"
-    };
-
-    onLanguageChange = lang => {
-        this.setState({ language: lang });
-    };
-
     render() {
         return (
             <div className="ui container">
-                <LanguageSelector onLanguageChange={this.onLanguageChange} />
-                <ColorContext.Provider value="red">
-                    <LanguageContext.Provider value={this.state.language}>
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value="red">
                         <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
--- context/src/context/LanguageContext.js
+++ context/src/context/LanguageContext.js
@@ -1,4 +1,29 @@
 import React from "react";
 
 // "en" is the default value of the context object
-export default React.createContext("en");
+const Context = React.createContext("en");
+
+export class LanguageStore extends React.Component {
+    state = {
+        language: "en"
+    };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    };
+
+    render() {
+        return (
+            <Context.Provider
+                value={{
+                    ...this.state,
+                    onLanguageChange: this.onLanguageChange
+                }}
+            >
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+}
+
+export default Context;
